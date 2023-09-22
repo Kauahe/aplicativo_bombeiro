529d4ea84793af6dad7f69052074431734ef8471
Kauahe <kaua_mira@estudante.sesisenai.org.br>
2023-09-22 08:20:20 -0300

    gabriel

--- php/verifica.php
+++ php/verifica.php
@@ -1,30 +1,24 @@
 <?php
+session_start();
 
-  session_start();
- 
-  $codigo = $_POST["codigo"];
-  $codigo2 = $_POST["codigo2"];
-  $codigo3 = $_POST["codigo3"];
-  $codigo4 = $_POST["codigo4"];
-  
-  include("conecta.php");
-  
-  $query = "SELECT * FROM cadastro WHERE codigo = :codigo and codigo2 = :codigo2 and codigo3 = :codigo3 and codigo4 = :codigo4";
-  $statement = $pdo->prepare($query);
-  $statement->bindParam(":codigo", $codigo);
-  $statement->bindParam(":codigo2", $codigo2);
-  $statement->bindParam(":codigo3", $codigo3);
-  $statement->bindParam(":codigo4", $codigo4);
-  $statement->execute();
-  
-  $matchingCodes = $statement->fetchAll(PDO::FETCH_ASSOC);
-  
-  if (count($matchingCodes) > 0) {
-      // Redirecionar para a página desejada
-      header("Location:../animacao.html");
-      exit();
-  } else {
-      // Exibir mensagem de erro
-      echo "Os códigos estão incorretos.";
-  }
-  ?>
+// Obtenha a senha do campo "senha"
+$senha = $_POST["senha"];
+
+include("conecta.php");
+
+$query = "SELECT * FROM cadastro WHERE senha = :senha";
+$statement = $pdo->prepare($query);
+$statement->bindParam(":senha", $senha);
+$statement->execute();
+
+$matchingCodes = $statement->fetchAll(PDO::FETCH_ASSOC);
+
+if (count($matchingCodes) > 0) {
+    // Redirecionar para a página desejada
+    header("Location:../animacao.html");
+    exit();
+} else {
+    // Exibir mensagem de erro
+    echo "Os códigos estão incorretos.";
+}
+?>
