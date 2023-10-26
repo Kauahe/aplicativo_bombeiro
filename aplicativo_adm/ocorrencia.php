1d5269065b91fcf94ba46e41944374718fc4f1af
kauhe <kaua_mira@estudante.sesisenai.org.br>
2023-10-26 09:41:45 -0300

    PAGINA DE OCORRENCIA EM ANDAMENTO

---
+++ aplicativo_adm/ocorrencia.php
@@ -0,0 +1,97 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE-edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Ocorrencias</title>
+    <link rel="stylesheet" href="css/ocorrencia.css">
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Audiowide">
+    <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
+</head>
+<body>
+    <header>
+        <div class="espaco">
+            <div class="imagem">
+                <img src="../img/logosembrilho.png" width="60px" height="60px">
+            </div>
+            <div class="row">
+                <h1>SOS BOMBEIROS</h1>
+                <h2>NÚCLEO DE OPERAÇÕES AÉREAS E RESGATE</h2>
+            </div>
+        </div>
+    </header>
+    <div class="titulo"><b>OCORRÊNCIAS </b></div>
+    <div class="caixa">
+        <div class="cdt">
+            <div class="add">
+                <form>
+                    <a href="cadastro.html" class="btn"><b>LISTAR POR</b></a>
+                </form>
+               
+            </div>
+        
+        </div>
+        <div class="linha"></div>
+        <div class="cadastros">
+            <?php
+            // Inclua o arquivo de conexão com o banco de dados
+            include("../php/conecta.php");
+
+            // Função para listar usuários
+            function listarOcorrencias($pdo) {
+                $sql = "SELECT num_ocorrencia, nome_pac, data_ocr FROM cadastropac";
+                $stmt = $pdo->query($sql);
+
+                if ($stmt->rowCount() > 0) {
+                    while ($row = $stmt->fetch()) {
+                        echo "<div class='barra' >";
+                        echo "<div class='info-usuario'data-id='" . $row["num_ocorrencia"] . "' onclick='redirectToProfile(this)'>";
+                        echo "<div class='info-id'> " . $row["num_ocorrencia"] . "</div>";
+                        echo "<div class='info-nome'> " . $row["nome_pac"] . "</div>";
+                        echo "<div class='info-email'> " . $row["data_ocr"] . "</div>";
+                        echo "</div>";
+                        echo "<div class='excluir' data-userid='" . $row["num_ocorrencia"] . "'>Excluir</div>";
+                        echo "</div>";
+                    }
+                } else {
+                    echo "Nenhuma ocorrencia encontrada .";
+                }
+            }
+
+            // Listar usuários
+            listarOcorrencias($pdo);
+            ?>
+        </div>
+    </div>
+</body>
+<script>
+$(document).ready(function () {
+    $('.cadastros_pac').on('click', '.excluir', function () {
+        var userId = $(this).data('userid');
+        if (confirm('Tem certeza de que deseja excluir esta ococrrencia?')) {
+            $.ajax({
+                url: 'php/excluir_ocr.php',
+                method: 'POST',
+                data: { id: userId },
+                success: function (data) {
+                    // Atualize a lista de usuários após a exclusão
+                    listarOcorrencias();
+                    alert(data); // Exibe uma mensagem com o resultado da exclusão
+                }
+            });
+        }
+    });
+
+    // Função para listar usuários novamente após a exclusão
+    function listarOcorrencias() {
+        $.ajax({
+            url: 'php/listar_ocorrencias.php', // Crie um arquivo PHP para listar usuários
+            method: 'GET',
+            success: function (data) {
+                $('.cadastros_pac').html(data); // Atualize a lista de usuários
+            }
+        });
+    }
+});
+</script>
---
+++ aplicativo_adm/php/excluir_ocr.php
@@ -0,0 +1,23 @@
+<?php
+// Inclua o arquivo de conexão com o banco de dados
+include("../../php/conecta.php");
+
+if (isset($_POST['num_ocorrencia'])) {
+    $num_ocorrencia = $_POST['num_ocorrencia'];
+
+    // Evite injeção de SQL usando declarações preparadas
+    $sql = "DELETE FROM cadastro_pac WHERE num_ocorrencia = :num_ocorrencia";
+    $stmt = $pdo->prepare($sql);
+    $stmt->bindParam(':num_ocorrencia', $num_ocorrencia, PDO::PARAM_INT);
+
+    if ($stmt->execute()) {
+        // A exclusão foi bem-sucedida
+        echo "ocorrencia excluído com sucesso.";
+    } else {
+        // Erro na exclusão
+        echo "Erro ao excluir a ocorrencia.";
+    }
+} else {
+    echo "num_ocorrencia não fornecido.";
+}
+?>
