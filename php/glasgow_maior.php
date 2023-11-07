b357cdf352b9062e3a3e6f8873f77a5398f58a5b
Kauahe <kaua_henrique@estudante.sesisenai.org.br>
2023-11-07 12:21:42 -0300

    evelin0711

--- php/glasgow_maior.php
+++ php/glasgow_maior.php
@@ -1,7 +1,7 @@
 <?php
 include("conecta.php");
 session_start();
-$_SESSION["v"]= "v";
+$_SESSION["v3"]= "v";
    $num_ocorrencia=  $_SESSION["nrOcorrencia"];
    $opcao1=isset($_POST["opcao1"])  ? "Espontânea" : "-";
    $opcao2=isset($_POST["opcao2"])  ? "Comando Verbal" : "-";
