bf7438cc08629f73a1f0fea2ccf6cea19df4d81f
kauahe <kaua_mira@estudante.sesisenai.org.br>
2023-11-13 15:41:37 -0300

    KAUAZIN

--- php/gestacional.php
+++ php/gestacional.php
@@ -1,27 +1,33 @@
 <?php
 include("conecta.php");
 session_start();
-$_SESSION["v4"]= "v";
+$_SESSION["v3"]= "v";
    $num_ocorrencia=  $_SESSION["nrOcorrencia"];
    $opcao1 = $_POST["opcao1"];
    $opcao2=isset($_POST["opcao2"])  ? "Sim" : "-";
    $opcao3=isset($_POST["opcao3"])  ? "Não" : "-";
    $opcao4 = $_POST["opcao4"];
    $opcao5=isset($_POST["opcao5"])  ? "Sim" : "-";
-   $opcao6=isset($_POST["opcao6"])? "Não" : "-";
-   $opcao7 = $_POST["opcao7"];
-   $opcao8=isset($_POST["opcao8"]) ? "Sim " : "-";
-   $opcao9=isset($_POST["opcao9"]) ?  "Não " : "-";
+   $opcao6=isset($_POST["opcao6"])  ? "Não" : "-";
+   $opcao7=isset($_POST["opcao7"])  ? "Sim" : "-";
+   $opcao8=isset($_POST["opcao8"])  ? "Não" : "-";
+   $opcao9 = $_POST["opcao9"];
    $opcao10 = $_POST["opcao10"];
-   $opcao11=isset($_POST["opcao11"])  ? "Sim" : "-";
-   $opcao12=isset($_POST["opcao12"])  ? "Não " : "-";
-   $opcao13 = $_POST["opcao13"];
-   $opcao14=isset($_POST["opcao14"]) ? "Sim " : "-";
-   $opcao15=isset($_POST["opcao15"]) ? "Não " : "-";
-   $opcao16 = $_POST["opcao16"];
+   $opcao11 = $_POST["opcao11"];
+   $opcao12 = $_POST["opcao12"];
+   $opcao13=isset($_POST["opcao13"])  ? "Sim" : "-";
+   $opcao14=isset($_POST["opcao14"])  ? "Não" : "-";
+   $opcao15=isset($_POST["opcao15"])  ? "Sim" : "-";
+   $opcao16=isset($_POST["opcao16"])  ? "Não" : "-";
+   $opcao17=isset($_POST["opcao17"])  ? "Sim" : "-";
+   $opcao18=isset($_POST["opcao18"])  ? "Não" : "-";
+   $opcao19=isset($_POST["opcao19"])  ? "Sim" : "-";
+   $opcao20=isset($_POST["opcao20"])  ? "Não" : "-";
+   $opcao21 = $_POST["opcao21"];
+   
   
    
-   $sql = "INSERT INTO anamnese VALUES (:id, :num_ocorrencia, :op1, :op2, :op3, :op4, :op5, :op6, :op7, :op8, :op9, :op10, :op11, :op12, :op13, :op14, :op15, :op16)";
+   $sql = "INSERT INTO gestacional VALUES (:id, :num_ocorrencia, :op1, :op2, :op3, :op4, :op5, :op6, :op7, :op8, :op9, :op10, :op11, :op12, :op13, :op14, :op15, :op16, :op17, :op18, :op19, :op20, :op21 )";
     $stmt = $pdo->prepare($sql);
    $n=null;
     $stmt->bindParam(':id', $n);
@@ -42,7 +48,12 @@ $_SESSION["v4"]= "v";
     $stmt->bindParam(':op14', $opcao14);
     $stmt->bindParam(':op15', $opcao15);
     $stmt->bindParam(':op16', $opcao16);
-
+    $stmt->bindParam(':op17', $opcao17);
+    $stmt->bindParam(':op18', $opcao18);
+    $stmt->bindParam(':op19', $opcao19);
+    $stmt->bindParam(':op20', $opcao20);
+    $stmt->bindParam(':op21', $opcao21);
+    
   
     $stmt->execute();
     
